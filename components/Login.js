9509ac56aeb61f1d1e0d21efc8754a88d4555b91
Alondrob <alondrob@gmail.com>
2022-02-27 01:27:16 -0500

    authentication

---
+++ components/Login.js
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+const Login = () => {
+  
+  
+ return (
+    <div>
+        {/* <Image
+            src="https://links.papareact.com" 
+            height={400}
+            width={400}
+            objectFit="contain"
+        /> */}
+
+        please login
+    </div>
+  )
+}
+
+export default Login
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Image from 'next/image'
-
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({session}) {
+  if(!session) return <Login/>
   return (
     <div >
       <Head>
@@ -14,4 +16,15 @@ export default function Home() {
       
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  //get the user
+  const session = await getSession(context);
+  
+  return {
+    props: {
+      session
+    }
+  }
 }
